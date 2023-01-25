8dfb7cf1a6d08d672235b775add5ca5b9ac96796
Alexia E. Smith <washuu@gmail.com>
2023-01-25 14:19:25 +0900

    Initial commit.

---
+++ config.h
@@ -0,0 +1,10 @@
+#define SW_LIMIT_F_PIN 47
+#define SW_LIMIT_B_PIN 49
+#define SW_LIMIT_U_PIN 45
+#define SW_LIMIT_D_PIN 53
+#define SW_LIMIT_L_PIN 51
+
+#define DEFAULT_SPEED_FB 255   // 0-255
+#define DEFAULT_SPEED_LR 255   // 0-255
+#define DEFAULT_SPEED_UD 255   // 0-255
+#define DEFAULT_SPEED_CLAW 255 // 0-255
---
+++ openufo.ino
@@ -0,0 +1,230 @@
+#include "config.h"
+#include <AFMotor.h>
+
+int LIMIT_F = 0;
+int LIMIT_B = 0;
+int LIMIT_U = 0;
+int LIMIT_D = 0;
+int LIMIT_L = 0;
+
+AF_DCMotor MT_UD(1, MOTOR34_8KHZ);
+AF_DCMotor CLAW(2, MOTOR34_8KHZ);
+AF_DCMotor MT_FB(3, MOTOR34_8KHZ);
+AF_DCMotor MT_LR(4, MOTOR34_8KHZ);
+
+bool clawParked = false;
+bool gantryParked = true;
+
+void setup() {
+	pinMode(SW_LIMIT_F_PIN, INPUT_PULLUP); // Gantry Forward Limit
+	pinMode(SW_LIMIT_B_PIN, INPUT_PULLUP); // Gantry Backward Limit
+	pinMode(SW_LIMIT_U_PIN, INPUT_PULLUP); // Claw Up Limit
+	pinMode(SW_LIMIT_D_PIN, INPUT_PULLUP); // Claw Down Limit
+	pinMode(SW_LIMIT_L_PIN, INPUT_PULLUP); // Gantry Left Limit
+	readLimitSwitches();				   // Get the initial limit switch state.
+
+	Serial.begin(115200);
+	setDefaultSpeed();
+	clack();
+
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+	readLimitSwitches();
+	Serial.print(LIMIT_F);
+	Serial.print(LIMIT_B);
+	Serial.print(LIMIT_U);
+	Serial.print(LIMIT_D);
+	Serial.print(LIMIT_L);
+	Serial.println();
+	delay(100);
+	parkAll();
+}
+
+void readLimitSwitches() {
+	LIMIT_F = digitalRead(SW_LIMIT_F_PIN);
+	LIMIT_B = digitalRead(SW_LIMIT_B_PIN);
+	LIMIT_U = digitalRead(SW_LIMIT_U_PIN);
+	LIMIT_D = digitalRead(SW_LIMIT_D_PIN);
+	LIMIT_L = digitalRead(SW_LIMIT_L_PIN);
+}
+
+// CLACK!  Releases tension on the claw string and does a claw check.
+void clack() {
+	moveUD(-1);
+	delay(1000);
+	moveUD(0);
+	moveClaw(1);
+	delay(100);
+	moveClaw(0);
+}
+
+// Sets a default speed for all motors.
+void setDefaultSpeed() {
+	MT_UD.setSpeed(DEFAULT_SPEED_UD);
+	MT_FB.setSpeed(DEFAULT_SPEED_FB);
+	MT_LR.setSpeed(DEFAULT_SPEED_LR);
+	CLAW.setSpeed(DEFAULT_SPEED_CLAW);
+}
+
+bool parkAll() {
+	// Park the claw first so it does not crash into anything while parking the gantry.
+	if (!parkClaw()) {
+		return false;
+	}
+	if (!parkGantry()) {
+		return false;
+	}
+	return true;
+}
+
+bool parkClaw() {
+	unsigned long startMillis = millis();
+	unsigned long currentMillis = 0;
+	while (!isULimitTriggered()) {
+		readLimitSwitches();
+		if (!isULimitTriggered()) {
+			moveUD(1);
+		} else {
+			moveUD(0);
+		}
+
+		currentMillis = millis();
+		if (!isULimitTriggered() && currentMillis - startMillis >= 2000) {
+			// Error state, took too long.
+			// Don't break, return.  We don't want to attempt parking the gantry.
+			return false;
+		}
+	}
+	clawParked = true;
+	return true;
+}
+
+bool parkGantry() {
+	unsigned long startMillis = millis();
+	unsigned long currentMillis = 0;
+	while (!isFLimitTriggered() || !isLLimitTriggered()) {
+		readLimitSwitches();
+
+		if (!isFLimitTriggered()) {
+			moveFB(1);
+		} else {
+			moveFB(0);
+		}
+
+		if (!isLLimitTriggered()) {
+			moveLR(-1);
+		} else {
+			moveLR(0);
+		}
+
+		currentMillis = millis();
+		if ((!isFLimitTriggered() || !isLLimitTriggered()) && currentMillis - startMillis >= 2000) {
+			// Error state, took too long.
+			return false;
+		}
+	}
+	gantryParked = true;
+	return true;
+}
+
+bool isFLimitTriggered() {
+	return LIMIT_F == 0;
+}
+
+bool isBLimitTriggered() {
+	return LIMIT_B == 0;
+}
+
+bool isLLimitTriggered() {
+	return LIMIT_L == 0;
+}
+
+bool isULimitTriggered() {
+	return LIMIT_U == 0;
+}
+
+bool isDLimitTriggered() {
+	return LIMIT_D == 0;
+}
+
+// 1 = Forwards towards the player.
+// 0 = Stop
+//-1 = Backwards away from the player.
+void moveFB(int dir) {
+	switch (dir) {
+		case 1: // Forward
+			if (!isFLimitTriggered()) {
+				MT_FB.run(BACKWARD); // I'm not swapping the wires.  *Angry elf noises.*
+			}
+			break;
+		case -1: // Backward
+			if (!isBLimitTriggered()) {
+				MT_FB.run(FORWARD);
+			}
+			break;
+		case 0: // Stop
+		default:
+			MT_FB.run(RELEASE);
+			break;
+	}
+}
+
+// 1 = Right towards the right side of the machine.
+// 0 = Stop
+//-1 = Left towards the left side of the machine.
+void moveLR(int dir) {
+	switch (dir) {
+		case 1: // Right
+			// No limit switch installed.
+			MT_LR.run(BACKWARD);
+			break;
+		case -1: // Left
+			if (!isLLimitTriggered()) {
+				MT_LR.run(FORWARD);
+			}
+			break;
+		case 0: // Stop
+		default:
+			MT_LR.run(RELEASE);
+			break;
+	}
+}
+
+// 1 = Up
+// 0 = Stop
+//-1 = Down
+void moveUD(int dir) {
+	switch (dir) {
+		case 1: // Up
+			if (!isULimitTriggered()) {
+				MT_UD.run(BACKWARD);
+			}
+			break;
+		case -1: // Down
+			if (!isDLimitTriggered()) {
+				MT_UD.run(FORWARD);
+			}
+			break;
+		case 0: // Stop
+		default:
+			MT_UD.run(RELEASE);
+			break;
+	}
+}
+
+// 1 = Close
+// 0 = Open
+void moveClaw(int state) {
+	// Forward and backward closes the claw.
+	switch (state) {
+		case 1: // Close
+			CLAW.run(BACKWARD);
+			break;
+		case 0: // Stop
+		default:
+			CLAW.run(RELEASE);
+			break;
+	}
+}
